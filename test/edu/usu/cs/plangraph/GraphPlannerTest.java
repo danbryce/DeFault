70e603d5becc675c07136e0db1f97d7266f2d2bf
dbryce <dbryce@db5f4523-d333-0410-b191-92aa9660ebcb>
2009-09-16 23:34:02 +0000

    git-svn-id: https://cmxt.sri.com/svn/bl/graphplanner/trunk@5852 db5f4523-d333-0410-b191-92aa9660ebcb

--- test/edu/usu/cs/plangraph/GraphPlannerTest.java
+++ test/edu/usu/cs/plangraph/GraphPlannerTest.java
@@ -125,52 +125,52 @@ public class GraphPlannerTest {
 
 	}
 	
-	@Test
-	public void test_PSPDomainTest() {
-
-		try {
-
-			File domainFileOut = getFile("/PSP/mable-control3.pddl");
-			File problemFileOut = getFile("/PSP/TBDName.pddl");
-
-			//			if (!domainFileOut.exists()) 
-			//				logger.debug("Unable to find PDDL domain file " + domainFileString);
-			//			if (!problemFileOut.exists())
-			//				logger.debug("Unable to find PDDL problem file " + problemFileString);
-
-			ANTLRDomainBuilder domBuilder = new ANTLRDomainBuilder(domainFileOut);
-			Domain domain = domBuilder.buildDomain();
-			ANTLRProblemBuilder probBuilder = new ANTLRProblemBuilder(domain, problemFileOut);
-			Problem problem = probBuilder.buildProblem();
-
-			SearchStatistics searchStatistics = new SearchStatistics();
-			SolverOptions solverOptions = new SolverOptions();
-			Solver solver = new PSPSolver(domain, problem, searchStatistics, solverOptions);
-			List<ActionInstance> plan = solver.run();
-			if (plan == null) {
-				System.out.println("No plan found");
-			} else {
-				//System.out.println("Plan found in " + solver.getNumLevels() + " levels:");
-				for (ActionInstance action : plan) {
-					System.out.println(action.toString());
-				}
-			}
-
-			Assert.assertNotNull(plan);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (PDDLSyntaxException e) {
-			e.printStackTrace();
-		} catch (InvalidPDDLElementException e) {
-			e.printStackTrace();
-		} catch (IllDefinedProblemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-	
+//	@Test
+//	public void test_PSPDomainTest() {
+//
+//		try {
+//
+//			File domainFileOut = getFile("/PSP/mable-control3.pddl");
+//			File problemFileOut = getFile("/PSP/TBDName.pddl");
+//
+//			//			if (!domainFileOut.exists()) 
+//			//				logger.debug("Unable to find PDDL domain file " + domainFileString);
+//			//			if (!problemFileOut.exists())
+//			//				logger.debug("Unable to find PDDL problem file " + problemFileString);
+//
+//			ANTLRDomainBuilder domBuilder = new ANTLRDomainBuilder(domainFileOut);
+//			Domain domain = domBuilder.buildDomain();
+//			ANTLRProblemBuilder probBuilder = new ANTLRProblemBuilder(domain, problemFileOut);
+//			Problem problem = probBuilder.buildProblem();
+//
+//			SearchStatistics searchStatistics = new SearchStatistics();
+//			SolverOptions solverOptions = new SolverOptions();
+//			Solver solver = new PSPSolver(domain, problem, searchStatistics, solverOptions);
+//			List<ActionInstance> plan = solver.run();
+//			if (plan == null) {
+//				System.out.println("No plan found");
+//			} else {
+//				//System.out.println("Plan found in " + solver.getNumLevels() + " levels:");
+//				for (ActionInstance action : plan) {
+//					System.out.println(action.toString());
+//				}
+//			}
+//
+//			Assert.assertNotNull(plan);
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (PDDLSyntaxException e) {
+//			e.printStackTrace();
+//		} catch (InvalidPDDLElementException e) {
+//			e.printStackTrace();
+//		} catch (IllDefinedProblemException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
+//	
 	
 	//Utils
 	private File getFile(String fileString){
