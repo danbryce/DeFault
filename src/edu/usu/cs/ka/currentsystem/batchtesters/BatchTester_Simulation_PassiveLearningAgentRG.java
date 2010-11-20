9fa2e0abc88847843e95de5d807f350b675fa244
cweber <cweber@db5f4523-d333-0410-b191-92aa9660ebcb>
2010-11-20 00:32:29 +0000

    committing not working...
    git-svn-id: https://cmxt.sri.com/svn/bl/graphplanner/branches/chrisw@8894 db5f4523-d333-0410-b191-92aa9660ebcb

--- src/edu/usu/cs/ka/currentsystem/batchtesters/BatchTester_Simulation_PassiveLearningAgentRG.java
+++ src/edu/usu/cs/ka/currentsystem/batchtesters/BatchTester_Simulation_PassiveLearningAgentRG.java
@@ -20,15 +20,15 @@ public class BatchTester_Simulation_PassiveLearningAgentRG
 		System.out.println("&: fail in future.\n");
 		
 		//Just mess around with the for loop values...
-		for(int gridSize = 2; gridSize <= 4; gridSize*=2) //2-32
+		for(int gridSize = 4; gridSize <= 4; gridSize*=2) //2-32
 		{
-			for(double bridgeDensity = 0.0; bridgeDensity <= 1.0; bridgeDensity += 0.25) //0.0 - 1.0
+			for(double bridgeDensity = 0.5; bridgeDensity <= 1.0; bridgeDensity += 0.25) //0.0 - 1.0
 			{	
-				for(int version = 1; version <= 3; version++)//1-3
+				for(int version = 1; version <= 1; version++)//1-3
 				{
-					for(int numFile = 1; numFile <= 10; numFile++) //numFiles 1-10
+					for(int numFile = 1; numFile <= 1; numFile++) //numFiles 1-10
 					{
-						for(int simSeed = 0; simSeed < 10000 && Simulation_PassiveLearningAgentRG.numSuccesses < 10; simSeed++)
+						for(int simSeed = 3; simSeed < 4 && Simulation_PassiveLearningAgentRG.numSuccesses < 10; simSeed++)
 						{	
 							args[0] = pathToDomains + "bridges_v" + version + "_" + gridSize + "_" + bridgeDensity + "_" + numFile + extension;
 							args[1] = pathToProblems + "bridges_problem" + extension;
